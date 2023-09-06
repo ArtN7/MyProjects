2f9ec0a3ef11c76403ec23c754383ea749819540
ArtN7 <nam_artem_2004@mail.ru>
2023-09-06 22:28:01 +0300

    finish first page. UPD product.html

--- Planet/index.js
+++ Planet/index.js
@@ -1,13 +1,15 @@
 const popup = document.querySelector('.popup');
 const buttonClose = popup.querySelector('.popup-button-close');
-const buttonOrder= document.querySelector('.button-order');
+const buttonOrder = document.querySelectorAll('.button-order');
 const body = document.querySelector('body');
 
-buttonOrder.addEventListener('click', () => {
-    popup.classList.remove('popup-display-none');
-    body.classList.add('popup-is-work')
-})
+for (const button of buttonOrder) {
+    button.addEventListener('click', () => {
+        popup.classList.remove('popup-display-none');
+        body.classList.add('popup-is-work');
+    })
+}
 buttonClose.addEventListener('click', () => {
     popup.classList.add('popup-display-none');
-    body.classList.remove('popup-is-work')
+    body.classList.remove('popup-is-work');
 })
