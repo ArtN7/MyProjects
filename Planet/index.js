a6a62aadd9818c2d141bcd33da25763d276420f9
ArtN7 <nam_artem_2004@mail.ru>
2023-09-10 20:58:41 +0300

    UPD third page

--- Planet/index.js
+++ Planet/index.js
@@ -1,8 +1,9 @@
 const popup = document.querySelector('.popup');
-const buttonClose = popup.querySelector('.popup-button-close');
+const buttonClose = document.querySelectorAll('.popup-button-close');
 const buttonOrder = document.querySelectorAll('.button-order');
 const buttonSend = document.querySelector('.popup-button-send');
 const body = document.querySelector('body');
+const popupConfirmation = document.querySelector('.popup-confirmation');
 
 for (const button of buttonOrder) {
     button.addEventListener('click', () => {
@@ -10,7 +11,11 @@ for (const button of buttonOrder) {
         body.classList.add('popup-is-work');
     })
 }
-buttonClose.addEventListener('click', () => {
-    popup.classList.add('popup-display-none');
-    body.classList.remove('popup-is-work');
+buttonSend.addEventListener('click', () => {
 })
+for (const button of buttonClose) {
+    button.addEventListener('click', () => {
+        popup.classList.add('popup-display-none');
+        body.classList.remove('popup-is-work');
+    })
+}
