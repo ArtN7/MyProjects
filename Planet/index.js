580308e3cc1aea1a2106e15266fd09163f72bed2
ArtN7 <nam_artem_2004@mail.ru>
2023-09-11 21:38:30 +0300

    finish first and second page. UPD third page

--- Planet/index.js
+++ Planet/index.js
@@ -1,21 +1,29 @@
-const popup = document.querySelector('.popup');
+const popup = document.querySelector('.popup-order');
 const buttonClose = document.querySelectorAll('.popup-button-close');
 const buttonOrder = document.querySelectorAll('.button-order');
-const buttonSend = document.querySelector('.popup-button-send');
+const buttonSubmit = document.querySelector('.popup-submit');
 const body = document.querySelector('body');
 const popupConfirmation = document.querySelector('.popup-confirmation');
-
+const buttonOK = popupConfirmation.querySelector('.button-ok');
 for (const button of buttonOrder) {
     button.addEventListener('click', () => {
         popup.classList.remove('popup-display-none');
         body.classList.add('popup-is-work');
     })
 }
-buttonSend.addEventListener('click', () => {
+buttonSubmit.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    popup.classList.add('popup-display-none');
+    popupConfirmation.classList.remove('popup-confirmation-order');
 })
 for (const button of buttonClose) {
     button.addEventListener('click', () => {
         popup.classList.add('popup-display-none');
         body.classList.remove('popup-is-work');
+        popupConfirmation.classList.add('popup-confirmation-order');
     })
 }
+buttonOK.addEventListener('click', () => {
+    popupConfirmation.classList.add('popup-confirmation-order');
+    body.classList.remove('popup-is-work');
+})
